2b0e0f011ed9063f1f86c7296a570fa0d105f83b
acoromin <acoromin@224674b8-e365-4e73-a4a8-558dbbfec58c>
2015-01-07 14:35:59 +0000

    New example added based on  non-template classes for ceres wrapping

---
+++ src/examples/test_ceres_wrapper_non_template.cpp
@@ -0,0 +1,187 @@
+// Testing a full wolf tree avoiding template classes for NodeLinked derived classes
+
+//std includes
+#include <iostream>
+#include <vector>
+// #include <memory>
+// #include <random>
+// #include <typeinfo>
+
+//ceres
+#include "ceres/ceres.h"
+
+//Wolf includes
+#include "wolf.h"
+#include "node_terminus.h"
+#include "node_linked.h"
+
+//forward declarations
+class TrajectoryBaseX;
+class FrameBaseX;
+class CaptureBaseX;
+class FeatureBaseX;
+class CorrespondenceBaseX;
+
+//class TrajectoryBaseX
+class TrajectoryBaseX : public NodeLinked<NodeTerminus,FrameBaseX>
+{
+    protected:
+        unsigned int length_; //just something to play
+        
+    public:
+        TrajectoryBaseX(const unsigned int _len) :
+            NodeLinked(TOP, "TRAJECTORY"),
+            length_(_len)
+        {
+            //
+        };
+        
+        ~TrajectoryBaseX()
+        {
+            
+        };
+};
+
+//class FrameBaseX
+class FrameBaseX : public NodeLinked<TrajectoryBaseX,CaptureBaseX>
+{
+    protected:
+        double time_stamp_; //frame ts
+        
+    public:
+        FrameBaseX(double _ts) :
+            NodeLinked(MID, "FRAME"),
+            time_stamp_(_ts)
+        {
+            //
+        };
+        
+        ~FrameBaseX()
+        {
+            
+        };
+};
+
+//class CaptureBaseX
+class CaptureBaseX : public NodeLinked<FrameBaseX,FeatureBaseX>
+{
+    protected:
+        double time_stamp_; //capture ts
+        
+    public:
+        CaptureBaseX(double _ts) :
+            NodeLinked(MID, "CAPTURE"),
+            time_stamp_(_ts)
+        {
+            //
+        };
+        
+        ~CaptureBaseX()
+        {
+            
+        };
+};
+
+//class FeatureBaseX
+class FeatureBaseX : public NodeLinked<CaptureBaseX,CorrespondenceBaseX>
+{
+    protected:
+        
+    public:
+        FeatureBaseX() :
+            NodeLinked(MID, "FEATURE")
+        {
+            //
+        };
+        
+        ~FeatureBaseX()
+        {
+            //
+        };
+};
+
+//class CorrespondenceBaseX
+class CorrespondenceBaseX : public NodeLinked<FeatureBaseX,NodeTerminus>
+{
+    protected:
+        unsigned int nblocks_; //number of state blocks in which the correspondence depends on.
+        std::vector<unsigned int> block_indexes_; //state vector indexes indicating start of each state block. This vector has nblocks_ size. 
+        std::vector<unsigned int> block_sizes_; //sizes of each state block. This vector has nblocks_ size. 
+        
+        
+    public:
+        CorrespondenceBaseX(const unsigned int _nb, const std::vector<unsigned int> & _szs) :
+            NodeLinked(BOTTOM, "CORRESPONDENCE"),
+            nblocks_(_nb),
+            block_sizes_(_szs)
+        {
+            assert(block_sizes_.size() == nblocks_);
+        };
+        
+        ~CorrespondenceBaseX()
+        {
+            //
+        };
+};
+
+//an example of a specialized corrspondence class
+class CorrespondenceOdom2D : public CorrespondenceBaseX
+{
+    protected:
+        Eigen::Map<Eigen::Vector3s> pose_previous_;
+        Eigen::Map<Eigen::Vector3s> pose_current_; 
+        ceres::CostFunction* cost_function_ptr_;
+        
+    public:
+        CorrespondenceOdom2D(const std::vector<unsigned int> & _idx, double * _st) :
+            CorrespondenceBaseX(2,std::vector<unsigned int>{3,3}),
+            pose_previous_(_st + block_indexes_.at(0) , block_sizes_.at(0)),
+            pose_current_(_st + block_indexes_.at(1) , block_sizes_.at(1))
+        {
+            //
+        };
+        
+        ~CorrespondenceOdom2D()
+        {
+            //
+        };
+        
+        void init()
+        {
+            std::cout << "pose_previous_ = " << pose_previous_.transpose() << std::endl;
+            std::cout << "pose_current_ = " << pose_current_.transpose() << std::endl;
+        };
+    
+};
+
+int main(int argc, char** argv) 
+{
+    //Welcome message
+    std::cout << " ========= WOLF-CERES test with non-template classes ===========" << std::endl << std::endl;
+
+    //variables
+    Eigen::VectorXs state;
+    std::vector<unsigned int> indexes;
+    CorrespondenceOdom2D *odom_corresp;
+        
+    //set state
+    indexes = {0,3};
+    state << 1,2,3,4,5,6;
+    
+    //create odom correspondence
+    odom_corresp = new CorrespondenceOdom2D(indexes, state.data());
+//     odom_corresp->init();
+    
+
+    //free memory
+    delete odom_corresp;
+    
+    //End message
+    std::cout << " =========================== END ===============================" << std::endl << std::endl;
+       
+    //exit
+    return 0;
+}
+
+
+
